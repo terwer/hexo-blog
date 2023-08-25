03e55db368803eb4ee5c635e7c8ebe958253af85
zhanyuecs <yuezhangcs@163.com>
2023-08-25 22:16:10 +0800

    feat: add dark mode

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # hexo-blog
 
 my blog via hexo
+
+## Dev
+
+hexo
+
+```
+pnpm dev
+```
+
+style
+
+```
+pnpm devStyle
+```
--- source/custom/custom.js
+++ source/custom/custom.js
@@ -1,1 +1,31 @@
-console.log("hello")
+/**
+ * 自定义js
+ *
+ * @author terwer
+ */
+$(document).ready(function () {
+    console.log("hello")
+    const navbarEnd = document.querySelector(".navbar-end");
+
+    const newLink = document.createElement("a");
+    newLink.classList.add("navbar-item");
+    newLink.setAttribute("title", "Toggle Dark Mode");
+    newLink.addEventListener("click", toggleDarkMode);
+
+    const icon = document.createElement("i");
+    icon.classList.add("fas", "fa-moon");
+
+    newLink.appendChild(icon);
+    navbarEnd.appendChild(newLink);
+
+    function toggleDarkMode() {
+        const htmlElement = document.documentElement;
+        const currentMode = htmlElement.getAttribute('data-theme-mode');
+
+        if (currentMode && currentMode === 'dark') {
+            htmlElement.setAttribute('data-theme-mode', 'light');
+        } else {
+            htmlElement.setAttribute('data-theme-mode', 'dark');
+        }
+    }
+});
